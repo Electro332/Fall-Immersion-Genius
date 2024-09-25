b0613fddbcb96caf34b898b0edf7ea661526ffb2
Electro332 <timothy.davis@hgs.hiddengeniusproject.org>
2024-09-24 18:49:57 -0700

    Update script.js

--- CS/javascript/model-pop-up/script.js
+++ CS/javascript/model-pop-up/script.js
@@ -1,3 +1,24 @@
-function modelPopUp() {
-    // Your code here.
-}   
+//get modal element
+const modal = document.getElementById("myModal");
+
+//get buttons 
+const openModalButton = document.getElementById("openModalBtn");
+const closeModalBtn = document.getElementById("closeModalBtn");
+const alertBtn = document.getElementById("alertBtn");
+
+//Open Modal on button click
+openModalButton.onclick = function() {
+    modal.style.display = "block";
+}
+
+//close Modal on outside click 
+window.onclick = function(event){
+    if (event.target === modal) {
+        modal.style.display = "none"
+    }
+};
+
+//show alert on button click
+alertBtn.onclick = function() {
+    alert("Test")
+};
