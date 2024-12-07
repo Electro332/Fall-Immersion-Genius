3fbae96566fe8dce0ed753b3f618133f753ccc9f
Electro332 <timothy.davis@hgs.hiddengeniusproject.org>
2024-12-07 13:50:59 -0800

    Add files via upload

--- CS/flask/Intro-to-Flask/intro-to-flask.py
+++ CS/flask/Intro-to-Flask/intro-to-flask.py
@@ -1,1 +1,17 @@
-# instruction will below. 
+#app.py
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/pants')
+def about():
+    return render_template('pants.html')
+
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
